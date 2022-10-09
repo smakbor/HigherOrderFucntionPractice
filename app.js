abbea38acfee92789f182f5dcb9c75609deccdab
smakbor <smakbar3965@gmail.com>
2022-10-10 00:02:18 +0600

    test

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const users = [
   { firstName: "SM", lastName: "Akbor", age: 24 },
-  { firstName: "Rezwan", lastName: "Shamim", age: 13 },
+  { firstName: "Rezwan", lastName: "Shamim", age: 14 },
   { firstName: "Yeakub", lastName: "Ali", age: 24 },
-  { firstName: "Jahid", lastName: "Hasan", age: 22 },
-  { firstName: "Fahim", lastName: "Khan", age: 21 },
-  { firstName: "Shahin", lastName: "Alam", age: 13 },
+  { firstName: "Jahid", lastName: "Hasan", age: 13 },
+  { firstName: "Fahim", lastName: "Khan", age: 13 },
+  { firstName: "SM", lastName: "Alam", age: 14 },
   { firstName: "Sagor", lastName: "Zaman", age: 20 },
-  { firstName: "Jahid", lastName: "Hasan", age: 22 },
-  { firstName: "Rejaul", lastName: "Karim", age: 13 },
+  { firstName: "SM", lastName: "Hasan", age: 13 },
+  { firstName: "Rejaul", lastName: "Karim", age: 14 },
 ];
 
 const newUser = users.reduce((acc, curr) => {
